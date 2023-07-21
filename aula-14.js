8e62b22fc3d4fe15e7bda722a55bcb18c3781d7e
Marllon Rodrigues <marllonrsantos@gmail.com>
2023-07-21 00:06:51 -0300

    feat: aula14

---
+++ aula-14.js
@@ -0,0 +1,13 @@
+const mensagem = 'vjksdgbvsd sdkjhbgsa'
+const mensagem2 = 'jhsbghjsdkvshdvjs'
+
+console.log('Tamanho: ', mensagem.length);
+console.log('Concat', mensagem.concat(mensagem2));
+console.log('Concat +',"jdkshgs" + "" + mensagem + "" + mensagem2);
+console.log('ES:', `sdgsdag ${mensagem} ${mensagem2}`);
+console.log('Quebrar em array', mensagem.split(' '));
+
+console.log('UP:', mensagem.toUpperCase());
+console.log('DOWN:', mensagem.toLocaleLowerCase());
+console.log('Replace? ', mensagem.replace('sdkjhbgsa', 'JULIO'));
+
