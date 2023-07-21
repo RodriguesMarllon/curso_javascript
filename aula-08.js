48089cab76cf6a853d901eb23789a1486302c058
Marllon Rodrigues <marllonrsantos@gmail.com>
2023-07-20 23:04:25 -0300

    feat: aula08

---
+++ aula-08.js
@@ -0,0 +1,7 @@
+const salario ='103';
+const salarioNumero = Number(salario);
+
+
+console.log('salario:', salario);
+console.log('salario:', salarioNumero);
+console.log('salario numero:', salarioNumero.toFixed(2));
