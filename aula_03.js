fd141e3253ea89b5f8dfad53247a6a27fc35596d
Marllon Rodrigues <marllonrsantos@gmail.com>
2023-07-20 21:05:45 -0300

    feat: aula03

---
+++ aula_03.js
@@ -0,0 +1,11 @@
+const idade = 28;
+const nome = 'Julio';
+let podeEntrar = false;
+
+if (idade <= 28) {
+    podeEntrar = true;
+} else {
+    console.log ('Não entra!!!')
+}
+
+console.log('Usuário pode entrar', podeEntrar)
