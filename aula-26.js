20791081e4b32cfbef2f6e817a67c329fc83d7ab
Marllon Rodrigues <marllonrsantos@gmail.com>
2023-07-22 23:25:37 -0300

    feat: aula26

---
+++ aula-26.js
@@ -0,0 +1,44 @@
+const lista = [
+    {
+        nome: 'Jose',
+        idade: 13
+    },
+    {
+        nome: 'Joao',
+        idade: 42       
+    },
+    {
+        nome: 'Maria',
+        idade: 23
+    },
+    {
+        nome: 'Fernanda',
+        idade: 65
+    },
+    {
+        nome: 'Andre',
+        idade: 69
+    },
+    {
+        nome: 'Heitor',
+        idade: 32
+    },
+    {
+        nome: 'Gustavo',
+        idade: 64
+    },
+    {
+        nome: 'Sandra',
+        idade: 33
+    }
+]
+
+const converterObjeto = (objeto) => {
+    return {
+        ...objeto,
+        idade: `${objeto.nome} + ${objeto.idade}`
+    }
+    
+}
+
+console.log(lista.map(converterObjeto));
