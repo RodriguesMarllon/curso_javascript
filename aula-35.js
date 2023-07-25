03b769b375d1b13784356ae4b41425a7da5c5845
Marllon Rodrigues <marllonrsantos@gmail.com>
2023-07-24 23:05:01 -0300

    feat: aula35

---
+++ aula-35.js
@@ -0,0 +1,30 @@
+class Pessoa {
+    nome; 
+    idade;
+    filhos;
+
+    quantosFilhos() {
+        if (this.filhos) {
+            return this.filhos.lenght;
+        }
+        return 0;
+    }
+
+    constructor(nome, idade, filhos) {
+        this.nome = nome;
+        this.idade = idade;
+        this.filhos = filhos;
+    }
+
+}
+
+
+
+const filho1 = new Pessoa('Gustavo', 13);
+const filho2 = new Pessoa('Gabriela', 10);
+console.log('filho1 tem: ', filho1.quantosFilhos());
+
+const pessoa = new Pessoa('Joao', 54, [filho1, filho2]);
+
+console.log('Quantos filhos? ', pessoa.quantosFilhos());
+console.log(pessoa);
